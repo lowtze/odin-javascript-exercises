56463c6595e8fba242bca02be27f09111eadbe99
lowtze <lowtze@protonmail.com>
2022-10-23 12:13:37 -0500

    Add solution to ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayName, ...removeMe) {
 
-};
+  const args = [...removeMe];
+
+  arrayName = arrayName.filter(function (val) {
+    return args.indexOf(val) == -1;
+  });
+
+  return arrayName;
+}
+//for item in array
+//if item is also in args
+//remove item
 
 // Do not edit below this line
 module.exports = removeFromArray;
