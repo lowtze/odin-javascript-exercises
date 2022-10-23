42b633e146904c2cb95b451cedc57877f1108821
lowtze <lowtze@protonmail.com>
2022-10-23 12:38:02 -0500

    Add solution ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+
+  if (Number.isInteger(int1) && Number.isInteger(int2)) {
+
+    if (int1 > 0 && int2 > 0) {
+      let finalSum = 0;
+      if (int1 > int2) {
+        for (let i = int2; i <= int1; i++) {
+          finalSum += i;
+        }
+      } else if (int2 > int1) {
+        for (let i = int1; i <= int2; i++) {
+          finalSum += i;
+        }
+      }
+      return finalSum;
+    } else {
+      return "ERROR";
+    }
+  } else {
+    return "ERROR";
+  }
 
 };
 
